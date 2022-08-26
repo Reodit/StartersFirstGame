db8d074ebe17d5e61c3e348ae7d9f17d114b3a9d
Reodit <dino9525@naver.com>
2022-08-26 12:37:44 +0900

    싱글톤 1차 커밋

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+    private GameManager()
+    {
+        // 외부에서 생성하지 못하도록 private로 생성
+        // 변수 넣어주기
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance = null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    public void GameOver()
+    {
+        EditorApplication.isPlaying = true;
+        //Application.Quit();
+    }
+
+    public void GamePause()
+    {
+        EditorApplication.isPaused = true;
+    }
+}
