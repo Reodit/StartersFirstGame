cd54f4d73dc302dddc49eeccdeaea8979c185c75
Reodit <dino9525@naver.com>
2022-08-26 15:10:58 +0900

    싱글톤 2차 커밋
    enemy, getgold 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,14 @@ using UnityEditor;
 public class GameManager : MonoBehaviour
 { 
     private static GameManager instance = null;
+    private Player mPlayer;
+    private Enemy mEnemy;
     private static int mScore;
     private static float mTime;
     private static int mEnemyCount;
     private static int mStage;
+    private static int mGold;
+    private static int mCurrentGold;
 
     private GameManager()
     {
@@ -56,5 +60,27 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         mTime = Time.time;
+        if(CheckEnemyDie(Enemy.mEnemyGroup) == true)
+        {
+            GetGold(mEnemy);
+        }
+    }
+
+    public void GetGold(Enemy enemy)
+    {
+        mCurrentGold += enemy.rewardGold;
+    }
+
+    private bool CheckEnemyDie(Enemy[] enemies)
+    {
+        for (int i = 0; i < enemies.GetLength(0); ++i)
+        {
+            if (enemies[i].Hp <= 0)
+            {
+                mEnemy = enemies[i];
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    Player player;
+    GameManager instance;
+
+    void Start()
+    {
+       GameObject myPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //상점 UI 열
+        }
+    }
+}
